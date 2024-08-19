097bc0d1ffd9ab7bc6e085842891387e4ad67882
Sree Deekshitha Yerra <126445402+SreeEswaran@users.noreply.github.com>
2024-08-19 22:54:07 +0530

    Create training.py

---
+++ src/training.py
@@ -0,0 +1,16 @@
+from transformers import Trainer, TrainingArguments
+
+def fine_tune_model(model, train_dataset, eval_dataset):
+    training_args = TrainingArguments(
+        output_dir="./results",
+        per_device_train_batch_size=4,
+        num_train_epochs=3,
+        logging_dir="./logs",
+    )
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset
+    )
+    trainer.train()
