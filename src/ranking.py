28fb217a59680cb078b2169a650be8b25d6aac1e
Sree Deekshitha Yerra <126445402+SreeEswaran@users.noreply.github.com>
2024-10-22 23:22:42 +0530

    Update ranking.py

--- src/ranking.py
+++ src/ranking.py
@@ -1,6 +1,5 @@
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
-
 def rank_responses(responses, query_embedding, model):
     response_embeddings = [model.encode(resp) for resp in responses]
     similarities = cosine_similarity([query_embedding], response_embeddings)
